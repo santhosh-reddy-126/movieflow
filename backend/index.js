6cfb519c05df5c18d89e9a12f837a0e14eadde1e
Santhosh Reddy <santhoshbeeram19@gmail.com>
2024-11-15 21:48:30 +0530

    database basic setup

---
+++ backend/index.js
@@ -0,0 +1,51 @@
+import express from "express";
+import cors from "cors";
+
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://ybnkyixugojfqjlssrvi.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlibmt5aXh1Z29qZnFqbHNzcnZpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzE2ODQ5NjMsImV4cCI6MjA0NzI2MDk2M30._mqawcOHTawwkQFiPn_l4YtU3CsykFvhB4JwdxV1dCk";
+const supabase = createClient(supabaseUrl, supabaseKey);
+const app = express();
+const port = 3144;
+app.use(express.json());
+app.use(cors());
+
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
+
+async function fetchData() {
+    const { data, error } = await supabase
+      .from("hello")
+      .select("*");
+  
+    if (error) {
+      console.error("Error:", error);
+    } else {
+      console.log("Fetched data:", data);
+    }
+  }
+  
+  fetchData();
+app.get("/", (req, res) => {
+  res.send("<h1>Hello,I am Server</h1>");
+});
+app.listen(port, () => {
+  console.log("Server Running on port " + port);
+});
